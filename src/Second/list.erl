16ac7d2bb5e235e009daeeca2ed7af49192b41c0
Maks <makst@DESKTOP-66M8DAV>
2020-09-27 23:07:22 +0300

    List changes. Added filter, reverse and changed reverse_create function.

--- src/Second/list.erl
+++ src/Second/list.erl
@@ -1,11 +1,21 @@
 -module(list).
 -author("makst").
--export([create/1, reverse_create/1]).
+-export([create/1, reverse_create/1, filter/2, reverse/1]).
 
 create(N) -> create(N, []).
 create(0, Res) -> Res;
 create(N, Res) -> create(N-1, [N|Res]).
 
-reverse_create(N) -> reverse_create(N, []).
-reverse_create(0, Res) -> Res;
-reverse_create(N, Res) -> reverse_create(N-1, Res ++ [N]).
+reverse_create(N) -> reverse(create(N)).
+
+filter([], _) -> [];
+filter([H | T], Limit) ->
+  if
+    H =< Limit -> [H|filter(T, Limit)];
+    true -> filter(T, Limit)
+  end.
+
+reverse([]) -> [];
+reverse(List) ->
+  [H | T] = List,
+  reverse(T) ++ [H].
